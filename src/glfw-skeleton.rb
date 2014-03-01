fe65ddc09c66520fa6bcb17592a39ec763eea543
Andy Stanton <andystanton@gmail.com>
2014-03-01 22:57:25 +0000

    Use gltranslate rather than offsetting coords

--- src/glfw-skeleton.rb
+++ src/glfw-skeleton.rb
@@ -7,110 +7,113 @@ Glfw::Window.window_hint(Glfw::RESIZABLE, 0)
 window = Glfw::Window.new(640, 480, "GLFW Skeleton Ruby")
 
 def drawRubyAt(pos_x, pos_y)
-  glBegin GL_QUADS
-    glColor3f(0.5, 0.0, 0.0)
-    glVertex2f(pos_x - 5, pos_y + 5)
-    glVertex2f(pos_x - 5, pos_y - 5)
-    glVertex2f(pos_x - 7.5, pos_y - 5)
-    glVertex2f(pos_x - 7.5, pos_y + 5)
+    glPushMatrix
+        glTranslatef(pos_x, pos_y, 0)
+        glBegin GL_QUADS
+            glColor3f(0.5, 0, 0)
+            glVertex2f(-5, 5)
+            glVertex2f(-5, -5)
+            glVertex2f(-7.5, -5)
+            glVertex2f(-7.5, 5)
 
-    glColor3f(1.0, 0.0, 0.0)
-    glVertex2f(pos_x + 5, pos_y + 5)
-    glVertex2f(pos_x + 5, pos_y - 5)
-    glVertex2f(pos_x - 5, pos_y - 5)
-    glVertex2f(pos_x - 5, pos_y + 5)
+            glColor3f(1, 0, 0)
+            glVertex2f(5, 5)
+            glVertex2f(5, -5)
+            glVertex2f(-5, -5)
+            glVertex2f(-5, 5)
 
-    glColor3f(1.0, 0.5, 0.5)
-    glVertex2f(pos_x + 5, pos_y + 5)
-    glVertex2f(pos_x + 5, pos_y - 5)
-    glVertex2f(pos_x + 7.5, pos_y - 5)
-    glVertex2f(pos_x + 7.5, pos_y + 5)
+            glColor3f(1, 0.5, 0.5)
+            glVertex2f(5, 5)
+            glVertex2f(5, -5)
+            glVertex2f(7.5, -5)
+            glVertex2f(7.5, 5)
 
-    glColor3f(1.0, 0.8, 0.8)
-    glVertex2f(pos_x + 5, pos_y + 7.5)
-    glVertex2f(pos_x + 5, pos_y + 5)
-    glVertex2f(pos_x - 5, pos_y + 5)
-    glVertex2f(pos_x - 5, pos_y + 7.5)
+            glColor3f(1, 0.8, 0.8)
+            glVertex2f(5, 7.5)
+            glVertex2f(5, 5)
+            glVertex2f(-5, 5)
+            glVertex2f(-5, 7.5)
 
-    glColor3f(0.5, 0.0, 0.0)
-    glVertex2f(pos_x + 5, pos_y - 7.5)
-    glVertex2f(pos_x + 5, pos_y - 5)
-    glVertex2f(pos_x - 5, pos_y - 5)
-    glVertex2f(pos_x - 5, pos_y - 7.5)
-  glEnd
+            glColor3f(0.5, 0, 0)
+            glVertex2f(5, -7.5)
+            glVertex2f(5, -5)
+            glVertex2f(-5, -5)
+            glVertex2f(-5, -7.5)
+        glEnd
 
-  glBegin GL_TRIANGLES
-    glColor3f(0.5, 0.0, 0.0)
-    glVertex2f(pos_x - 5, pos_y + 7.5)
-    glVertex2f(pos_x - 5, pos_y + 5)
-    glVertex2f(pos_x - 7.5, pos_y + 5)
+        glBegin GL_TRIANGLES
+            glColor3f(0.5, 0, 0)
+            glVertex2f(-5, 7.5)
+            glVertex2f(-5, 5)
+            glVertex2f(-7.5, 5)
 
-    glColor3f(1.0, 0.0, 0.0)
-    glVertex2f(pos_x + 5, pos_y + 7.5)
-    glVertex2f(pos_x + 5, pos_y + 5)
-    glVertex2f(pos_x + 7.5, pos_y + 5)
+            glColor3f(1, 0, 0)
+            glVertex2f(5, 7.5)
+            glVertex2f(5, 5)
+            glVertex2f(7.5, 5)
 
-    glColor3f(0.3, 0.0, 0.0)
-    glVertex2f(pos_x - 5, pos_y - 7.5)
-    glVertex2f(pos_x - 5, pos_y - 5)
-    glVertex2f(pos_x - 7.5, pos_y - 5)
+            glColor3f(0.3, 0, 0)
+            glVertex2f(-5, -7.5)
+            glVertex2f(-5, -5)
+            glVertex2f(-7.5, -5)
 
-    glColor3f(0.5, 0.0, 0.0)
-    glVertex2f(pos_x + 5, pos_y - 7.5)
-    glVertex2f(pos_x + 5, pos_y - 5)
-    glVertex2f(pos_x + 7.5, pos_y - 5)
-
-  glEnd
+            glColor3f(0.5, 0, 0)
+            glVertex2f(5, -7.5)
+            glVertex2f(5, -5)
+            glVertex2f(7.5, -5)
+        glEnd
+    glPopMatrix
 end
 
 def drawSkullAt(pos_x, pos_y)
-    glPushMatrix()
-        glBegin(GL_QUADS)
-            glColor3f(1.0, 1.0, 1.0)
+    glPushMatrix
+        glTranslatef(pos_x, pos_y, 0)
+        glBegin GL_QUADS
+            glColor3f(1, 1, 1)
 
-            glVertex2f(pos_x + 60, pos_y + 50)
-            glVertex2f(pos_x + 60, pos_y - 40)
-            glVertex2f(pos_x - 60, pos_y - 40)
-            glVertex2f(pos_x - 60, pos_y + 50)
-    
-            glVertex2f(pos_x - 30, pos_y - 55)
-            glVertex2f(pos_x - 30, pos_y - 40)
-            glVertex2f(pos_x + 30, pos_y - 40)
-            glVertex2f(pos_x + 30, pos_y - 55)
-    
-            glColor3f(0.0, 0.0, 0.0)
-            
-            glVertex2f(pos_x - 10, pos_y + 0)
-            glVertex2f(pos_x - 10, pos_y - 30)
-            glVertex2f(pos_x - 40, pos_y - 30)
-            glVertex2f(pos_x - 40, pos_y + 0)
-            
-            glVertex2f(pos_x + 40, pos_y + 0)
-            glVertex2f(pos_x + 10, pos_y + 0)
-            glVertex2f(pos_x + 10, pos_y - 30)
-            glVertex2f(pos_x + 40, pos_y - 30)
-        glEnd()
-    
-        glBegin(GL_TRIANGLES)
-            glVertex2f(pos_x, pos_y - 30)
-            glVertex2f(pos_x + 10, pos_y - 40)
-            glVertex2f(pos_x - 10, pos_y - 40)
-        glEnd()
+            glVertex2f(60, 50)
+            glVertex2f(60, -40)
+            glVertex2f(-60, -40)
+            glVertex2f(-60, 50)
 
-        drawRubyAt(pos_x - 25, pos_y - 15)
-        drawRubyAt(pos_x + 25, pos_y - 15)
-    glPopMatrix()
+            glVertex2f(-30, -55)
+            glVertex2f(-30, -40)
+            glVertex2f(30, -40)
+            glVertex2f(30, -55)
+
+            glColor3f(0, 0, 0)
+
+            glVertex2f(-10, 0)
+            glVertex2f(-10, -30)
+            glVertex2f(-40, -30)
+            glVertex2f(-40, 0)
+
+            glVertex2f(40, 0)
+            glVertex2f(10, 0)
+            glVertex2f(10, -30)
+            glVertex2f(40, -30)
+        glEnd
+
+        glBegin GL_TRIANGLES
+            glVertex2f(0, -30)
+            glVertex2f(10, -40)
+            glVertex2f(-10, -40)
+        glEnd
+
+        drawRubyAt(-25, -15)
+        drawRubyAt(25, -15)
+    glPopMatrix
 end
 
-def reshape(window, width, height) 
-    w, h = window.framebuffer_size()
-    Gl.glViewport(0, 0, w, h)
-    Gl.glMatrixMode(GL_PROJECTION)
-    Gl.glLoadIdentity()
-    Gl.glScalef(1.0, -1.0, 1.0);
-    Gl.glOrtho(0, width, height, 0, 0, 1)
-    Gl.glMatrixMode(GL_MODELVIEW )
-    Gl.glLoadIdentity()
+def reshape(window, width, height)
+  w, h = window.framebuffer_size()
+  Gl.glViewport(0, 0, w, h)
+  Gl.glMatrixMode(GL_PROJECTION)
+  Gl.glLoadIdentity()
+  Gl.glScalef(1, -1, 1);
+  Gl.glOrtho(0, width, height, 0, 0, 1)
+  Gl.glMatrixMode(GL_MODELVIEW )
+  Gl.glLoadIdentity()
 end
 
 window.set_key_callback {
@@ -136,11 +139,11 @@ loop {
   Glfw.wait_events
   Gl.glClear(Gl::GL_COLOR_BUFFER_BIT | Gl::GL_DEPTH_BUFFER_BIT)
 
-  x,y = window.size
+  x, y = window.size
   drawSkullAt(x/2, y/2)
 
   window.swap_buffers
-  
+
   break if window.should_close?
 }
 
